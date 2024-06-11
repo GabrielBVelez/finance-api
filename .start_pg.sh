577ede56980668d3af2dc54e1ca50e089cb70261
Gabriel Velez <gabriel.velez@bemobi.com>
2024-06-10 23:23:39 -0300

    setup database.yml

--- .start_pg.sh
+++ .start_pg.sh
@@ -4,9 +4,9 @@ CONTAINER_NAME='finance_api_db'
 docker start $CONTAINER_NAME > /dev/null 2>&1 || \
   (
     docker run --name $CONTAINER_NAME \
-               -e POSTGRES_USER=social_mine \
+               -e POSTGRES_USER=finance_api \
                -e POSTGRES_PASSWORD='F1n@nc3_ap1' \
                -d \
-               -p 6789:6789 \
+               -p 5432:5432 \
                postgres:alpine
   )
